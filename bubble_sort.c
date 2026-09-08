830c81b679314a5dc4a5a04e2b4c9b2aa38f5f67
Shrey Modh <modhshrey@gmail.com>
2026-09-08 19:54:27 +0530

    Refactor bubble sort with new array and logic
    Updated bubble sort implementation to use a new array and improved variable names.

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
-int main(){
-    int A[5] = {5,3,8,2,4};
-    int n = 5, temp;
+int main(){//bubble sort
+    int arr[6] = {7,4,9,2,6,1};
 
-    for(int i = 0; i<n-1; i++){
-        for(int j = 0; j< n-i-1; j++){
-            if(A[j] > A[j+1]){
-
-                temp = A[j];
-                A[j] = A[j+1];//overwrite on A[j]
-                A[j+1] = temp;
-
+    //step-1: repeat step2 for i = 0 to i = n-2 times
+    for(int i = 0; i< 5; i++){
+        //step-2: repeat step-3 for j = 0 to j = n-i-1 times
+        for(int j = 0; j<6-i-1; j++){
+            /*step-3: if this*/
+            if(arr[j]> arr[j+1]){
+                //then swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
+        //step-4: exit
     }
 
-    //printing the sorted array
-    for(int i = 0; i < n; i++){
-        printf("%d ", A[i]);
+    //printing array
+    printf("modified array is: ");
+    for(int i = 0; i<6; i++){
+        printf("%d ", arr[i]);
     }
-    printf("\n");
 }
